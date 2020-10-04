e0180e85f06aadc4504abfe5d2c9792776f1cb01
Chitra-Gupta <chitrachanchal111@gmail.com>
2020-10-04 23:39:11 +0430

    Added Toast

--- app/src/main/java/chitra/helloworld/fragmentcalcheckbx/MainActivity.java
+++ app/src/main/java/chitra/helloworld/fragmentcalcheckbx/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button1=(Button) findViewById(R.id.button1);
         button2=(Button) findViewById(R.id.button2);
+        Toast.makeText(this, "Awesome", Toast.LENGTH_SHORT).show();
 
     }
     public void onClick(View view){
